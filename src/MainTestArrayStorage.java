444abc80efa2c889dc6af586c3e089e9317bba02
Nikita <ncheusov@inbox.ru>
2023-04-03 00:16:33 +0300

    initial commit

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -5,6 +5,7 @@ public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         Resume r1 = new Resume();
         r1.uuid = "uuid1";
         Resume r2 = new Resume();
