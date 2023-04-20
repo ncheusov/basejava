5069e559f74ebf774a046903cb0730818144e3d8
Nikita <ncheusov@inbox.ru>
2023-04-20 21:42:22 +0300

    "Implement hw1"

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,35 +6,44 @@ import java.util.Arrays;
 
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
         Arrays.fill(storage, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = storage[i + 1];
+                storage[i + 1] = storage[--size];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (!resume.equals(null)) {
-                size++;
-            }
-        }
         return size;
     }
 }
