a8145ead35815c29874e528823dc8c865c26231e
Nikita <ncheusov@inbox.ru>
2023-06-06 20:19:59 +0300

    Introduce binaryInsertSort

--- src/ru/javawebinar/basejava/MainTestArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -2,32 +2,35 @@ package ru.javawebinar.basejava;
 
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.SortedArrayStorage;
-import ru.javawebinar.basejava.storage.Storage;
 
 /**
  * Test for ru.javawebinar.basejava.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
 
-    private static final Storage ARRAY_STORAGE = new SortedArrayStorage();
+    private static final SortedArrayStorage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         final Resume r1 = new Resume();
-        r1.setUuid("uuid2");
+        r1.setUuid("uuid1");
         final Resume r2 = new Resume();
-        r2.setUuid("uuid4");
+        r2.setUuid("uuid2");
         final Resume r3 = new Resume();
         r3.setUuid("uuid3");
+        final Resume r4 = new Resume();
+        r4.setUuid("uuid4");
 
-        ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r1);
+//        System.out.println("Insert sort " + Math.abs(Arrays.binarySearch(ARRAY_STORAGE.storage, 0, ARRAY_STORAGE.size(), r4) + 1));
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
+        ARRAY_STORAGE.binaryInsertingSort();
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -6,6 +6,15 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
+    public void binaryInsertingSort() {
+        for (int i = 0; i < size; i++) {
+            Resume searchKey = storage[i];
+            int locationIndex = Math.abs(Arrays.binarySearch(storage, 0, i, searchKey) + 1);
+            System.arraycopy(storage, locationIndex, storage, locationIndex + 1, i - locationIndex);
+            storage[locationIndex] = searchKey;
+        }
+    }
+
     @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
