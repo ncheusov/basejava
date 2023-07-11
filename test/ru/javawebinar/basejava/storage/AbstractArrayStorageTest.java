b34c0c7453a6437f2c9fcebd05cf8859a4621070
Nikita <ncheusov@inbox.ru>
2023-07-11 20:58:43 +0300

    Introduce JUnit 4 tests

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -1,34 +1,57 @@
 package ru.javawebinar.basejava.storage;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
 
-class AbstractArrayStorageTest {
+public class AbstractArrayStorageTest {
 
-    @org.junit.jupiter.api.Test
-    void size() {
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private final Storage storage = new ArrayStorage();
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
     }
 
-    @org.junit.jupiter.api.Test
-    void update() {
+    @Test
+    public void size() throws Exception {
+        Assert.assertEquals(3, storage.size());
     }
 
-    @org.junit.jupiter.api.Test
-    void save() {
+    @Test
+    public void update() {
     }
 
-    @org.junit.jupiter.api.Test
-    void delete() {
+    @Test
+    public void save() {
     }
 
-    @org.junit.jupiter.api.Test
-    void clear() {
+    @Test
+    public void delete() {
     }
 
-    @org.junit.jupiter.api.Test
-    void getAll() {
+    @Test
+    public void clear() {
     }
 
-    @org.junit.jupiter.api.Test
-    void get() {
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() {
+        storage.get("dummy");
     }
 }
